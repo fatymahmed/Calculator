6b9a16dcdcff99d1d6e989f69bbd9f6db381786d
fatima ahmed <fatymah001@gmail.com>
2019-11-23 00:43:11 +0300

    Make group buttons align

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,31 +4,31 @@ import './ButtonPanel.css';
 
 const ButtonPanel = () => (
   <div className="ButtonPanel">
-    <div className="group1">
+    <div className="buttonGroup group1">
       <Button name="AC" />
       <Button name="+/-" />
       <Button name="%" />
       <Button name="/" />
     </div>
-    <div className="group2">
+    <div className="buttonGroup group2">
       <Button name="7" />
       <Button name="8" />
       <Button name="9" />
       <Button name="X" />
     </div>
-    <div className="group3">
+    <div className="buttonGroup group3">
       <Button name="4" />
       <Button name="5" />
       <Button name="6" />
       <Button name="-" />
     </div>
-    <div className="group4">
+    <div className="buttonGroup group4">
       <Button name="1" />
       <Button name="2" />
       <Button name="3" />
       <Button name="+" />
     </div>
-    <div className="group5">
+    <div className="buttonGroup group5">
       <Button name="0" />
       <Button name="." />
       <Button name="=" />
