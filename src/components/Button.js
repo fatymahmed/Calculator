7dd9c9bbc46b2ee6b290d84e722433ed9ea04ee6
fatima ahmed <fatymah001@gmail.com>
2019-11-24 11:18:28 +0300

    Add isWide property and style for '0' button

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,20 +5,31 @@ import './Button.css';
 const Button = (props) => {
   const { name } = props;
   const { color } = props;
+  const { isWide } = props;
   return (
-    <div className="Button" style={{ background: color }}>
-      <button type="button">{name}</button>
-    </div>
+    isWide
+      ? (
+        <div className="Button" style={{ width: '50%' }}>
+          <button type="button" style={{ backgroundColor: color }}>{name}</button>
+        </div>
+      )
+      : (
+        <div className="Button" style={{ width: '25%' }}>
+          <button type="button" style={{ backgroundColor: color }}>{name}</button>
+        </div>
+      )
   );
 };
 
 Button.propTypes = {
   name: PropTypes.string,
   color: PropTypes.string,
+  isWide: PropTypes.bool,
 };
 Button.defaultProps = {
   name: '',
   color: '#FF911A',
+  isWide: false,
 };
 
 export default Button;
