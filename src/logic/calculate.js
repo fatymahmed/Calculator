6fb7da47fe69b6256beb7d764449c43c179c4392
fatima ahmed <fatymah001@gmail.com>
2019-11-25 19:09:19 +0300

    Impelement calculate module

---
+++ src/logic/calculate.js
@@ -0,0 +1,12 @@
+import operate from 'operate';
+
+const calculate = (calculateObject, buttonName) => {
+  if (buttonName === '+/-') {
+    calculateObject.total *= -1;
+    calculateObject.next *= -1;
+  } else {
+    calculateObject.total = operate(calculateObject.total, calculateObject.next, buttonName);
+  }
+  return calculateObject;
+};
+export default calculate;
