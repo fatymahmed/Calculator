057bc932528800870c56e4ca4220090e95e5b73a
fatima ahmed <fatymah001@gmail.com>
2019-11-26 19:21:00 +0300

    Add computation with operators and numbers on display

--- src/components/App.js
+++ src/components/App.js
@@ -13,49 +13,64 @@ class App extends React.Component {
       next: null,
       operation: null,
       buttonName: null,
-      res: null,
+      display: null,
     };
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  clearAll() {
+    this.setState({
+      total: null,
+      next: null,
+      operation: null,
+      buttonName: null,
+      display: null,
+    });
+  }
+
+  clearAfterCalculation() {
+    this.setState({
+      next: null,
+      operation: null,
+      buttonName: null,
+    });
   }
 
   handleClick(e) {
     const content = e.target.textContent;
     console.log(Number(content));
     if (content === 'AC') {
-      this.setState({
-        total: null,
-        next: null,
-        operation: null,
-        buttonName: null,
-        res: null,
-      });
+      this.clearAll();
     } else if (content === '=') {
       this.setState((prevState) => (calculate(prevState, prevState.buttonName)));
-      this.setState((prevState) => ({ res: prevState.total }));
+      this.setState((prevState) => ({ display: prevState.total }));
+      this.clearAfterCalculation();
     } else if (isNaN(Number(content))) {
       this.setState({
         operation: content,
         buttonName: content,
       });
+      this.setState((prevState) => ({ display: prevState.display + content }));
     } else if (this.state.operation === null) {
       if (this.state.total === null) {
         this.setState({ total: content });
       } else {
         this.setState((prevState) => ({ total: prevState.total + content }));
       }
+      this.setState((prevState) => ({ display: prevState.total }));
     } else if (this.state.next === null) {
       this.setState({ next: content });
+      this.setState((prevState) => ({ display: prevState.display + content }));
     } else {
       this.setState((prevState) => ({ next: prevState.next + content }));
+      this.setState((prevState) => ({ display: prevState.display + content }));
     }
   }
 
   render() {
     return (
       <div className="app">
-        <Display result={this.state.res ? this.state.res
-          : (this.state.next ? this.state.next : this.state.total)}
-        />
+        <Display result={this.state.display ? this.state.display : '0'} />
         <ButtonPanel onClick={this.handleClick} />
       </div>
     );
